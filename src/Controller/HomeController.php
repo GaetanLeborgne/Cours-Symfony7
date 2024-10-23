9938e0593ff913143a4eb3f7d50538408b6d2612
GaetanLeborgne <gaetanleborgne.59400@hotmail.com>
2024-10-23 22:56:10 +0200

    Mise en place des premiers fichiers Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,18 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController{
 
     #[Route("/", name: "home")]
-    // Cette annotation définit une route pour l'URL racine ("/").
-    // Le nom de la route est "home", ce qui permet de l'identifier facilement dans l'application.
     
     function index (Request $request): Response {
-        // La méthode "index" accepte un objet "Request" en paramètre, qui représente la requête HTTP reçue par le serveur.
-    
-        dd($request);
-        // La fonction "dd()" (dump and die) est utilisée pour afficher le contenu de la requête et arrêter l'exécution du script,
-        // ce qui est utile pour le débogage.
-    
-        return new Response('Hello ' . $request->query->get('name', 'Inconnu'));
-        // Si la fonction "dd()" est supprimée, la méthode renverra une réponse contenant le texte "Hello ".
-        // Elle récupère la valeur du paramètre "name" dans la requête GET. Si ce paramètre n'est pas présent, elle renverra "Inconnu" par défaut.
+
+        return $this->render('home/index.html.twig');
     }
 }
