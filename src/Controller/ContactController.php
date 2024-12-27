dfaddb1280374cc1679c4455d6ac2887cbf6dcbf
GaetanLeborgne <gaetanleborgne.59400@hotmail.com>
2024-12-27 23:37:46 +0100

    mise en place du mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,28 +4,39 @@ namespace App\Controller;
 
 use App\DTO\ContactDTO;
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function contact(Request $request): Response
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $data = new ContactDTO();
 
         // TODO : A SUPPREMER A LA FIN DE LA FASE DE TEST
         $data->name = 'John Doe';
-        $data->email = 'john';
-        $data->message = 'supersite';
+        $data->email = 'john@doe.fr';
+        $data->message = 'super site';
 
 
         $form = $this->createForm(ContactType::class, $data);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            //Envoyer email
+            $mail = (new TemplatedEmail())
+                ->to('contact@demo.fr')
+                ->from($data->email)
+                ->subject('Demande de contact')
+                ->htmlTemplate ('emails/contact.html.twig')
+                ->context(['data' => $data]);
+            $mailer->send($mail);
+            $this->addFlash('success', 'Votre mail à bien était envoyé');
+            return $this->redirectToRoute('contact');
+
         }
         return $this->render('contact/contact.html.twig', [
             'form' => $form,
