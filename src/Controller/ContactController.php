6f4dce8ba91443a988f2338129307dcdc54a8a70
GaetanLeborgne <gaetanleborgne.59400@hotmail.com>
2024-12-27 22:23:12 +0100

    creation du controller et connection avec la vue

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\DTO\ContactDTO;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact(Request $request): Response
     {
+        $data = new ContactDTO();
+        $form = $this->createForm(ContactType::class, $data);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Envoyer email
+        }
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form,
         ]);
     }
 }
